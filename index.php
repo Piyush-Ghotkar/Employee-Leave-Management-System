39a699eb301b73c0f81e394e2b06b7bb8ad71e5d
Piyush Ghotkar <piyushghotkar@gmail.com>
2020-09-20 03:45:18 +0530

    adding-index.php

---
+++ index.php
@@ -0,0 +1,142 @@
+<?php
+session_start();
+error_reporting(0);
+
+?>
+<!DOCTYPE html>
+<html lang="en" >
+
+<head>
+  <meta charset="UTF-8">
+  <title>BIT</title>
+  <link href='https://fonts.googleapis.com/css?family=Dosis:400,200' rel='stylesheet' type='text/css'>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+  
+  <script src="js/homepage.js"></script>
+  <script src="js/modernizr-2.6.2.min.js"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  
+      <link rel="stylesheet" href="./style.css">
+
+
+</head>
+
+<body>
+        
+		<p class="item-1">LOGIN</p>
+  
+     
+    	 <!--  Apps Menu Selection –– -->
+		<div id="circle1" class="circle1 circle-border">
+		   <div class="wave wave1"></div>
+		   <div class="wave wave2"></div>
+		   <div class="wave wave3"></div>
+		   <a href="Principal/" id="ap" class="fill-div" >
+		     <div id="inner1" class="circle-inner">
+		      	<img id="45-512"  class="homeMenuIcon" src="index img/45-512.png"/>
+		         <div id="text1" class="score-text">
+		          	Principal
+		          </div>
+		     </div>
+		     </a>
+		</div>
+		<!--  Design Menu Selection –– -->
+		<div id="circle2" class="circle1 circle-border">
+			<div class="wave wave1"></div>
+		   <div class="wave wave2"></div>
+		   <div class="wave wave3"></div>
+		   <a href="HOD/" id="dn" class="fill-div" >
+		     <div id="inner2" class="circle-inner">
+		     	<img  id="male.png"class="homeMenuIcon" src="index img/male.png"/>
+		         <div id="text2" class="score-text">
+		            HOD
+		          </div>
+		     </div>
+		     </a>
+		</div>
+		<!--  Code Projects Selection –– -->
+		<div id="circle3" class="circle1 circle-border">
+			<div class="wave wave1"></div>
+		   <div class="wave wave2"></div>
+		   <div class="wave wave3"></div>
+		   <a href="admin/" id="ai" class="fill-div" >
+		     <div id="inner3" class="circle-inner">
+		     	<img id="303-3030193_leader-icon"class="homeMenuIcon" src="index img/303-3030193_leader-icon.png"/>
+		         <div id="text3" class="score-text">
+		            Admin
+		          </div>		  
+		     </div>
+		     </a>
+		</div>
+		<!--  Mindmap Menu Selection –– -->
+		<div id="circle4" class="circle1 circle-border">
+			<div class="wave wave1"></div>
+		   <div class="wave wave2"></div>
+		   <div class="wave wave3"></div>
+		   <a href="faculty_login.php" id="ab" class="fill-div" >
+		     <div id="inner4"class="circle-inner">
+		         <img id="036-512"class="homeMenuIcon" src="index img/036-512.png"/>
+		         <div id="text4" class="score-text">
+		            Faculty
+		          </div>
+		     </div>
+		     </a>
+		</div>
+		<div id="circle5" class="circle1 circle-border">
+			<div class="wave wave1"></div>
+		   <div class="wave wave2"></div>
+		   <div class="wave wave3"></div>
+		   <a href="administration/" id="ax" class="fill-div" >
+		     <div id="inner5"class="circle-inner">
+		         <img id="33882-200"class="homeMenuIcon" src="index img/33882-200.png"/>
+		         <div id="text5" class="score-text">
+		            Administration
+		          </div>
+		     </div>
+		     </a>
+		</div>
+		<div id="circle6" class="circle1 circle-border">
+			<div class="wave wave1"></div>
+		   <div class="wave wave2"></div>
+		   <div class="wave wave3"></div>
+		   <a href="sports_sch/" id="az" class="fill-div" >
+		     <div id="inner6"class="circle-inner">
+		         <img id="Education__seience_1_9-512"class="homeMenuIcon" src="index img/33882-200 12.png"/>
+		         <div id="text6" class="score-text">
+		            <font size="5px" style="position:relative;margin-left:-0px;display:in-line;">Sports/Stu.Section</font>
+		          </div>
+		     </div>
+		     </a>
+		</div>
+		<div id="circle7" class="circle1 circle-border">
+			<div class="wave wave1"></div>
+		   <div class="wave wave2"></div>
+		   <div class="wave wave3"></div>
+		    <a href="Student/" id="az" class="fill-div" >
+		     <div id="inner7"class="circle-inner">
+		         <img id="Education__seience_1_9-512"class="homeMenuIcon" src="index img/Education__seience_1_9-512.png"/>
+		         <div id="text7" class="score-text">
+		            Students
+		          </div>
+		     </div>
+		     </a>
+		</div>
+		</div>
+		</div> 
+		
+  </div>
+  <div>
+  
+  
+
+    <script  src="./script.js"></script>
+
+
+
+
+</body>
+
+</html>
