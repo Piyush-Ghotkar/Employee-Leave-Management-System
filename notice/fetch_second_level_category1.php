23563ad61e8dac946306951eeb9a4a33f43c0e50
Piyush Ghotkar <piyushghotkar@gmail.com>
2020-10-04 17:49:32 +0530

    fetch-2nd-backup

---
+++ notice/fetch_second_level_category1.php
@@ -0,0 +1,25 @@
+
+<?php
+
+//fetch_second_level_category.php
+
+$connect = new PDO('mysql:host=localhost;dbname=elms', 'root', '');
+
+if(isset($_POST["selected"]))
+{
+ $id = join("','", $_POST["selected"]);
+ $query = "
+ SELECT DepartmentName FROM tbldepartments where DepartmentName!='First Year Engineering'";
+ $statement = $connect->prepare($query);
+ $statement->execute();
+ $result = $statement->fetchAll();
+ $output = '';
+  $output .= '<option value="all_dept">All Departments of Selected Year</option>';
+ foreach($result as $row)
+ {
+  $output .= '<option value="'.$row["DepartmentName"].'">'.$row["DepartmentName"].'</option>';
+ }
+ echo $output;
+}
+
+?>
