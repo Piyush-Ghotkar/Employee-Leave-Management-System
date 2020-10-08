bdf7ed380c12baa07596feabb5877d306798116d
Piyush Ghotkar <piyushghotkar@gmail.com>
2020-10-08 19:13:05 +0530

    sidebar.php

---
+++ admin/includes/sidebar.php
@@ -0,0 +1,137 @@
+     <aside id="slide-out" class="side-nav white fixed">
+                <div class="side-nav-wrapper">
+                    <div class="sidebar-profile">
+                        <div class="sidebar-profile-image">
+                            <img src="../assets/images/profile-image.png" class="circle" alt="">
+                        </div>
+                        <div class="sidebar-profile-info">
+                       
+                                <p>Admin</p>
+
+                         
+                        </div>
+                    </div>
+            
+                <ul class="sidebar-menu collapsible collapsible-accordion" data-collapsible="accordion">
+                    <li class="no-padding"><a class="waves-effect waves-grey" href="dashboard.php"><i class="material-icons">settings_input_svideo</i>Dashboard</a></li>
+                  
+
+
+				   <li class="no-padding">
+                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">apps</i>Department<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="adddepartment.php">Add Department</a></li>
+                                <li><a href="managedepartments.php">Manage Department</a></li>
+                            </ul>
+                        </div>
+                    </li>
+					 
+					 <li class="no-padding">
+                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">beenhere</i>Leave Type</a>
+                           </li>
+					
+					<li class="no-padding">
+                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">local_activity</i>Designation<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="adddesignation.php">Add Designation</a></li>
+                                <li><a href="managedesignation.php">Manage Designation</a></li>
+                            </ul>
+                        </div>
+                    </li>
+				
+					<li class="no-padding">
+                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">account_box</i>HOD<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="addHOD.php">Add HOD</a></li>
+                                <li><a href="manageHOD.php">Manage HOD</a></li>
+       
+                            </ul>
+                        </div>
+                    </li>
+					
+                    <li class="no-padding">
+                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">account_box</i>Staff<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="addemployee.php">Add Staff Member</a></li>
+                                <li><a href="manageemployee.php">Manage Staff</a></li>
+       
+                            </ul>
+                        </div>
+                    </li>
+
+   <li class="no-padding">
+                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">desktop_windows</i>Leave Management<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+                        <div class="collapsible-body">
+                            <ul>
+							<li><a href="add_leave.php">Add Leaves to Staff</a></li>
+                                <li><a href="leaves.php">All Leaves </a></li>
+                                <li><a href="pending-leavehistory.php">Pending Leaves </a></li>
+                                <li><a href="approvedleave-history.php">Approved Leaves</a></li>
+                                  <li><a href="notapproved-leaves.php">Not Approved Leaves</a></li>
+       
+                            </ul>
+                        </div>
+                    </li>
+
+
+	<li class="no-padding">
+                        <a href="report/" class="collapsible-header waves-effect waves-grey"><i class="material-icons">event_note</i>Leaves Report</a>
+            </li>            
+
+		 <li class="no-padding">
+                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">library_books</i> Hall Ticket Clearence<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="HOD_clearence.php?year=1&show=all">First Year</a></li>
+								<li><a href="HOD_clearence.php?year=2&show=all">Second Year</a></li>
+                                <li><a href="HOD_clearence.php?year=3&show=all">Third Year</a></li>
+                                <li><a href="HOD_clearence.php?year=4&show=all">Fourth Year</a></li>
+                               <li><a href="dc_stu_clearance.php?show=all">DC Students</a></li>
+       
+                            </ul>
+                        </div>
+                    </li>				
+		
+				 <li class="no-padding">
+                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">library_books</i>Other Clearence<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="tc_clearance.php?show=all">Passout/TC Clearence</a></li>
+								<li><a href="transfer_clearance.php?show=all">Transfer Clearence</a></li>
+                                
+							</ul>
+                        </div>
+                    </li>
+					
+					
+				<li class="no-padding">
+                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">unfold_more</i>Promote/Demote Students<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="promote_demote.php?branch=Civil Engineering">Civil Engineering</a></li>
+								<li><a href="promote_demote.php?branch=Mechanical Engineering">Mechanical Engineering</a></li>
+                                <li><a href="promote_demote.php?branch=Computer Engineering">Computer Engineering</a></li>
+                                <li><a href="promote_demote.php?branch=Electrical Engineering">Electrical Engineering</a></li>
+							</ul>
+                        </div>
+                    </li>	
+					
+				<li class="no-padding"><a class="waves-effect waves-grey" href="changepassword.php"><i class="material-icons">settings_input_svideo</i>Change Password</a></li>
+
+                        <li class="no-padding">
+                                <a class="waves-effect waves-grey" href="logout.php"><i class="material-icons">exit_to_app</i>Sign Out</a>
+                            </li>  
+                 
+
+                 
+              
+                </ul>
+                   <div class="footer">
+                    
+                </div>
+                </div>
+            </aside>
