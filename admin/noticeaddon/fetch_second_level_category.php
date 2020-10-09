697497d3755ba8e83a7f21240655de8a6825e4a2
Piyush Ghotkar <piyushghotkar@gmail.com>
2020-10-09 18:28:38 +0530

    admin-2ndLevel-category

---
+++ admin/noticeaddon/fetch_second_level_category.php
@@ -0,0 +1,28 @@
+
+<?php
+
+//fetch_second_level_category.php
+
+include('../includes/config.php');
+
+if(isset($_POST["selected"]))
+{
+ $id = join("','", $_POST["selected"]);
+ $query = "
+ SELECT FirstName,LastName,EmpId FROM tblemployees 
+ WHERE Department IN ('".$id."')
+ ";
+ $statement = $connect->prepare($query);
+ $statement->execute();
+ $result = $statement->fetchAll();
+ $output = '';
+ 
+ $output .= '<option value="all_faculty">All Faculty of Selected Department</option>';
+ foreach($result as $row)
+ {
+  $output .= '<option value="'.$row["EmpId"].'">'.$row["FirstName"].'  '.$row["LastName"].'</option>';
+ }
+ echo $output;
+}
+
+?>
